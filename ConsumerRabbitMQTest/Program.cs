2c9ef73067eeca76403d988c094ad42e5e747785
Saed Mamedov <seid13463@gmail.com>
2024-12-21 18:34:18 +0400

    RabbitMQ Producer && Consumer

---
+++ ConsumerRabbitMQTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://zuduguoa:i3rDng9dw_Zfoa29DkCTQPr04FeDoexR@ostrich.lmq.cloudamqp.com/zuduguoa");
+using var connection = await factory.CreateConnectionAsync();
+
+var channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync("mystepqueue", durable: true, exclusive: false, autoDelete: false); // true burda onu bildirir ki, nese problem error hallari olanda restart verir <--- 
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+await channel.BasicConsumeAsync("mystepqueue",true,consumer);
+
+consumer.ReceivedAsync += Consumer_ReceivedAsync;
+
+Task Consumer_ReceivedAsync(object sender, BasicDeliverEventArgs @event)
+{
+    var message = Encoding.UTF8.GetString(@event.Body.ToArray());
+    Console.WriteLine(message);
+    return Task.CompletedTask;
+}
+
+Console.ReadLine();
---
+++ PublisherDirect/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://zuduguoa:i3rDng9dw_Zfoa29DkCTQPr04FeDoexR@ostrich.lmq.cloudamqp.com/zuduguoa");
+using var connection = await factory.CreateConnectionAsync();
+
+var channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync("mystepqueue", durable: true, exclusive: false, autoDelete: false); // true burda onu bildirir ki, nese problem error hallari olanda restart verir <--- 
+
+while (true)
+{
+    Console.Write("Enter your message for publishing : ");
+    var message = string.Concat("Message - ",Console.ReadLine(), " Time - ", DateTime.Now.ToLongTimeString());
+    var body = Encoding.UTF8.GetBytes(message);
+    await channel.BasicPublishAsync("", "mystepqueue", body);
+    await Console.Out.WriteLineAsync("Message sent succesfully .");
+    Console.ReadLine();
+}
